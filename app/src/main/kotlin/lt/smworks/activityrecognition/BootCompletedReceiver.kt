e888551896b9623e5b600514a743ebca5ee0887c
Martynas <martynas.su@gmail.com>
2025-06-01 14:14:00 +0300

    Added logic to start after restart. Also replaced startService with startForegroundService in main activity

--- app/src/main/kotlin/lt/smworks/activityrecognition/BootCompletedReceiver.kt
+++ app/src/main/kotlin/lt/smworks/activityrecognition/BootCompletedReceiver.kt
@@ -3,7 +3,6 @@ package lt.smworks.activityrecognition
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 
 class BootCompletedReceiver : BroadcastReceiver() {
 
