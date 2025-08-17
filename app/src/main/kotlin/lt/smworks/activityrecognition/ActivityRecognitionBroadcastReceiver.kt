a8e4f2da1704dd0a11b5a7f9d4de98137fd10cc6
Martynas <martynas.su@gmail.com>
2025-08-17 16:31:09 +0300

    Persist activity tracking state in SharedPreferences
    A new `SharedPreferencesProvider` class was introduced to manage the state of activity tracking.
    The `InVehicleForegroundService` now uses this provider to ensure that activity recognition is started only once and its state is persisted across service restarts.
    The `specification.txt` file was removed as it's no longer relevant.

--- app/src/main/kotlin/lt/smworks/activityrecognition/ActivityRecognitionBroadcastReceiver.kt
+++ app/src/main/kotlin/lt/smworks/activityrecognition/ActivityRecognitionBroadcastReceiver.kt
@@ -1,15 +1,22 @@
 package lt.smworks.activityrecognition
 
-import lt.smworks.activityrecognition.ActivityRecognitionEvent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import com.google.android.gms.location.ActivityRecognitionResult
+import com.google.android.gms.location.ActivityTransitionEvent
 import com.google.android.gms.location.ActivityTransitionResult
 import kotlinx.coroutines.DelicateCoroutinesApi
 import java.io.Serializable
 
 class ActivityRecognitionBroadcastReceiver : BroadcastReceiver() {
+
+    companion object {
+        private const val ACTIVITY_TRANSITION_DELAY = 10 * 60 * 1000L
+    }
+
+    private var lastEvent: ActivityTransitionEvent? = null
+    private var lastEventTimestamp: Long = 0
 
     @OptIn(DelicateCoroutinesApi::class)
     override fun onReceive(context: Context, intent: Intent) {
@@ -38,13 +45,54 @@ class ActivityRecognitionBroadcastReceiver : BroadcastReceiver() {
         val result = ActivityTransitionResult.extractResult(intent)
         FileLogger.d("handleActivityTransition(${result?.toTransitionEvents()})")
         if (result == null) {
+            FileLogger.w("Activity transition result is null")
             return
         }
+        val transitionEvents = result.transitionEvents
+        if (transitionEvents.isEmpty()) {
+            FileLogger.w("Activity transition events are empty")
+            return
+        }
+        if (transitionEvents.size == 1) {
+            handleSingleEventActivityTransition(context, transitionEvents[0])
+        } else if (result.transitionEvents.size == 2) {
+            handleTwoEventActivityTransition(context, transitionEvents)
+        } else {
+            FileLogger.w("More than two activity transition events detected!")
+        }
+    }
+
+    private fun handleSingleEventActivityTransition(context: Context, event: ActivityTransitionEvent) {
+        if (lastEvent != null) {
+            val isActivityTypeMatch = lastEvent?.activityType == event.activityType
+            val isTransitionTypeMatch = lastEvent?.transitionType == event.transitionType
+            val isDelayNotExceeded = System.currentTimeMillis() - lastEventTimestamp < ACTIVITY_TRANSITION_DELAY
+            if (isActivityTypeMatch && isTransitionTypeMatch && isDelayNotExceeded) {
+                FileLogger.i("Ignoring duplicate activity transition due to short delay")
+                return
+            }
+        }
+        lastEvent = event
+        lastEventTimestamp = System.currentTimeMillis()
+        startForegroundServiceWithNewActivityTransitions(context, listOf(event))
+    }
+
+    private fun handleTwoEventActivityTransition(
+        context: Context,
+        transitionEvents: List<ActivityTransitionEvent>
+    ) {
+        startForegroundServiceWithNewActivityTransitions(context, transitionEvents)
+    }
+
+    private fun startForegroundServiceWithNewActivityTransitions(
+        context: Context,
+        transitionEvents: List<ActivityTransitionEvent>
+    ) {
         val serviceIntent = Intent(context, InVehicleForegroundService::class.java).apply {
             action = InVehicleForegroundService.ACTION_ACTIVITY_TRANSITION_RECOGNISED
             putExtra(
                 InVehicleForegroundService.EXTRA_TRANSITION_EVENTS,
-                result.transitionEvents.map { transition ->
+                transitionEvents.map { transition ->
                     ActivityRecognitionEvent(
                         transition.activityType,
                         transition.transitionType
@@ -57,6 +105,8 @@ class ActivityRecognitionBroadcastReceiver : BroadcastReceiver() {
             FileLogger.e("Failed to start service for activity transition: ${t.message}")
         }
     }
+
+
 
     private fun handleActivity(activityType: Int, confidence: Int) {
         val activityName = activityType.getActivityName()
